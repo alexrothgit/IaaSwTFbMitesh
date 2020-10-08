7490a3e54a6d65ad4ee311af7d6f3a471688543c
AlexRoth <alexander.roth@accenture.com>
2020-10-08 15:32:29 +0200

    moved pw to secrets file

--- variables.tf
+++ variables.tf
@@ -3,15 +3,6 @@ variable "location" {
   default     = "centralus"
 }
 
-variable "username" {
-  description = "Username for Virtual Machines"
-  default     = "testadmin"
-}
-
-variable "password" {
-  description = "Password for Virtual Machines"
-  default     = "Password1234!"
-}
 
 variable "vmsize" {
   description = "Size of the VMs"
