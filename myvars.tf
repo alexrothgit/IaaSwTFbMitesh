799bf30278e17153025c4fb71ce9a9fac8ecadac
AlexRoth <alexander.roth@accenture.com>
2020-10-08 15:35:46 +0200

    new var file

---
+++ myvars.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "Location of the network"
+  default     = "centralus"
+}
+
+
+variable "vmsize" {
+  description = "Size of the VMs"
+  default     = "Standard_DS1_v2"
+}
